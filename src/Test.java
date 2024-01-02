548a783e0519e0318f84703050577879170adce3
Evgenygroup <mailfrom61@gmail.com>
2024-01-02 10:31:06 +0100

    new

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,7 @@
-import java.sql.SQLOutput;
-
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Nu in nu");
+        System.out.println("hello");
     }
+
 }
