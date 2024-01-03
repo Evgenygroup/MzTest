f8fea7e6398111f7b19ef4b70dc6d6e62fa28efa
Evgenygroup <mailfrom61@gmail.com>
2024-01-03 11:38:50 +0100

    null

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello 1st Branch and undo changes for revert");
+        System.out.println("hello 1st Branch and new commit");
     }
 
 }
